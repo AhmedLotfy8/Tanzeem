def510604b13001b41e82c0230ed09f572b4520f
Ahmed Lotfy <ahmedfert45@gmail.com>
2026-02-19 14:56:12 +0200

    Add project files.

---
+++ Tazneem.Persistence/Data/DbContexts/TanzeemDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tanzeem.Persistence.Data.DbContexts {
+    public class TanzeemDbContext(DbContextOptions<TanzeemDbContext> options) : DbContext {
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+    
+    }
+}

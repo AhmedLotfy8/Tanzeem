a7b831d0c2d4e72ff4292c5850ff32546629d1f2
Ahmed Lotfy <ahmedfert45@gmail.com>
2026-02-26 15:23:46 +0200

    Implemented: IGenericRepo, GenericRepo

---
+++ Tanzeem.Domain/Contracts/IGenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tanzeem.Domain.Contracts {
+    public interface IGenericRepository<Entity> where Entity : class {
+
+        Task<IEnumerable<Entity>> GetAllAsync();
+
+        Task<Entity?> GetByIdAsync(int id);
+
+        Task AddAsync(Entity entity);
+        
+        void UpdateAsync(Entity entity);
+
+        void DeleteAsync(Entity entity);
+
+
+    }
+}

b9ea200bbd44a9e4d598cf072451ad204d7b77b7
Ahmed Lotfy <ahmedfert45@gmail.com>
2026-03-05 00:59:42 +0200

    Company Entity modifications/Addition: 1. Added Attributes to Company Class 2. Modified Company's Configs 3. Created ICompanyService Class, CompanyDto

---
+++ Tanzeem.Shared/Dtos/Companies/CompanyDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tanzeem.Shared.Dtos.Companies {
+    public class CompanyDto {
+
+        public string Name { get; set; }
+
+        public string Field { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+    }
+}

050bb04dd4bd8ccbdb6c03eff78f0dffba1b2a5e
Ahmed Lotfy <ahmedfert45@gmail.com>
2026-04-09 17:40:23 +0200

    User Entity introduced: 1. User Entity 2. Updated company entity (for user relations) 3. user configs creation, comany configs modifications 4. Applied migrations

---
+++ Tanzeem.Persistence/Data/Configurations/UsersConfigurations/UserConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tanzeem.Domain.Entities.Users;
+
+namespace Tanzeem.Persistence.Data.Configurations.UsersConfigurations {
+    public class UserConfiguration : IEntityTypeConfiguration<User> {
+        public void Configure(EntityTypeBuilder<User> builder) {
+
+            builder.Property(x => x.Name)
+                .HasMaxLength(256);
+
+            builder.Property(x => x.Email)
+                .HasMaxLength(256);
+
+            builder.Property(x => x.PasswordHash)
+                .HasMaxLength(512);
+
+
+            builder.HasOne(x=> x.Company)
+                .WithMany(c => c.Users)
+                .HasForeignKey(x => x.CompanyId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+        }
+    }
+}

9776d63e5e05ce8a30dc6050fe61eb6ea45617a6
rewan <rewan@gmail.com>
2026-04-11 23:48:20 +0200

    CRUD Supplier and Order

--- Tanzeem.Persistence/Repositories/GenericRepository.cs
+++ Tanzeem.Persistence/Repositories/GenericRepository.cs
@@ -1,11 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Reflection.Metadata.Ecma335;
-using System.Text;
-using System.Threading.Tasks;
 using Tanzeem.Domain.Contracts;
 using Tanzeem.Persistence.Data.DbContexts;
 
